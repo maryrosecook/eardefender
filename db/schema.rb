cdc0f24530e666da3b31e93c53913f13e9bc64b7
maryrosecook <maryrosecook@maryrosecookhome-2.local>
2009-06-01 11:37:17 +0100

    db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 1) do
 
   create_table "scrobbles", :force => true do |t|
     t.string   "artist",  :limit => 1000
